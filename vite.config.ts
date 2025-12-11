834010c28bc838903b468d8a2e87c9e14b8c2148
Alex Fedortsov <oleksandr.fedortsov@mythical.games>
2025-12-11 13:48:25 +0200

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: './',
+    base: '/gin-site/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
